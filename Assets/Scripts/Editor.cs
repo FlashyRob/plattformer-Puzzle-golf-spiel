189f00db38cdd54ecbfbcf7002d982546c18c4ef
fkroker <fkroker@cjd-koenigswinter.net>
2025-06-24 13:36:40 +0200

    Add feature add sprites by click

--- Assets/Scripts/Editor.cs
+++ Assets/Scripts/Editor.cs
@@ -3,11 +3,12 @@ using UnityEngine;
 public class Editor : MonoBehaviour
 {
     public GameObject[] grass;
+
+    public Vector3 mousePos;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         Instantiate(grass[0], new Vector3(0, 0, 0), Quaternion.identity);
-        Instantiate(grass[0], new Vector3(1, 0, 0), Quaternion.identity);
     }
 
     // Update is called once per frame
@@ -15,7 +16,14 @@ public class Editor : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.Mouse0))
         {
-            Debug.Log(Camera.main.ScreenToWorldPoint(Input.mousePosition));
+            mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            mousePos = new Vector3(
+                Mathf.Round(mousePos.x),
+                Mathf.Round(mousePos.y),
+                0
+            );
+            Debug.Log(mousePos);
+            Instantiate(grass[0], mousePos, Quaternion.identity);
         }
     }
 }
