22282f7932f86807b6864ca70092ab72f516f8ba
Jazzpery <jaslue@outlook.de>
2025-06-25 10:17:07 +0200

    added wire updates

--- Assets/Scripts/Updates/Updates.cs
+++ Assets/Scripts/Updates/Updates.cs
@@ -1,27 +1,28 @@
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
 
 public class Updates : MonoBehaviour
 {
 
     public connectionData[] blocks = new connectionData[20];
-    public int[,] activeSides = new int[20,4];
+    public int[,] activeSides = new int[20, 4];
     public blockData[] blockData = new blockData[20];
 
 
-    
 
-    
+
+
 
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        for(int i = 0; i < blocks.Length; i++)
+        for (int i = 0; i < blocks.Length; i++)
         {
             var c = new connectionData();
             c.sides = new int[] { 0, 1, 2, 3 };
-            c.data = new Dictionary<int, List<connections>> { { 0, new List<connections>() }, { 1, new List<connections>()}, { 2, new List<connections>()}, { 3, new List<connections>()} };
+            c.data = new Dictionary<int, List<connections>> { { 0, new List<connections>() }, { 1, new List<connections>() }, { 2, new List<connections>() }, { 3, new List<connections>() } };
 
             blocks[i] = c;
         }
@@ -30,13 +31,13 @@ public class Updates : MonoBehaviour
         AddConnection(0, 0, new connections { outputIndex = 7, outputSide = 3 });
 
 
-        var l = GetConnections(0,0);
-        for(int i= 0; i < l.Count; i++)
+        var l = GetConnections(0, 0);
+        for (int i = 0; i < l.Count; i++)
         {
-            Debug.Log(l[i].outputIndex+ " " +l[i].outputSide);
+            Debug.Log(l[i].outputIndex + " " + l[i].outputSide);
         }
 
-        
+
     }
 
     // Update is called once per frame
@@ -46,10 +47,10 @@ public class Updates : MonoBehaviour
         {
             blockData block = blockData[i];
 
-            switch  (block.type)
+            switch (block.type)
             {
                 case "wire":
-                    handleWire(i, block);
+                    HandleWire(i, block);
                     break;
 
             }
@@ -60,7 +61,7 @@ public class Updates : MonoBehaviour
     {
         connectionData currentConnection = blocks[blockIndex];
         currentConnection.data[side].Add(connection);
-        
+
 
     }
 
@@ -70,46 +71,151 @@ public class Updates : MonoBehaviour
         return (l);
     }
 
-    public bool isActive(int index, int side)
+    public bool IsActive(int index, int side)
     {
         return (activeSides[index, side] == 1);
     }
 
-    public bool checkActive(List<connections> sources)
+    public bool CheckActive(List<connections> sources)
     {
         for (int i = 0; i < sources.Count; i++)
         {
             connections source = sources[i];
-            if (isActive(source.outputIndex, source.outputSide))
+            if (IsActive(source.outputIndex, source.outputSide))
                 return true;
 
         }
         return false;
     }
 
-    private void handleWire(int i, blockData block)
+    private void HandleWire(int i, blockData block)
     {
+        int[] activeConnections;
+
         switch (block.typetype)
         {
             case "wire_straight":
-                int[] activeConnections = checkConnectionSides(block.inputDirections, GetConnections(i, 0));
-                if (activeConnections == 1)
-                    {
-                        
-                    }
+                activeConnections = CheckConnectionSides(block.inputDirections, GetConnections(i, 1));
+                if (activeConnections.Contains(1))
+                {
+                    EditVisualActive(i, 1);
+                    break;
+                }
+
+
+                activeConnections = CheckConnectionSides(block.inputDirections, GetConnections(i, 3));
+                if (activeConnections.Contains(1))
+                {
+                    EditVisualActive(i, 1);
+                    break;
+                }
+                EditVisualActive(i, 0);
                 break;
-        } 
+
+            case "wire_curve":
+                activeConnections = CheckConnectionSides(block.inputDirections, GetConnections(i, 0));
+                if (activeConnections.Contains(1))
+                {
+                    EditVisualActive(i, 1);
+                    break;
+                }
+
+
+                activeConnections = CheckConnectionSides(block.inputDirections, GetConnections(i, 1));
+                if (activeConnections.Contains(1))
+                {
+                    EditVisualActive(i, 1);
+                    break;
+                }
+                break;
+
+            case "wire_t":
+                activeConnections = CheckConnectionSides(block.inputDirections, GetConnections(i, 0));
+                if (activeConnections.Contains(1))
+                {
+                    EditVisualActive(i, 1);
+                    break;
+                }
+
+                activeConnections = CheckConnectionSides(block.inputDirections, GetConnections(i, 1));
+                if (activeConnections.Contains(1))
+                {
+                    EditVisualActive(i, 1);
+                    break;
+                }
+
+                activeConnections = CheckConnectionSides(block.inputDirections, GetConnections(i, 3));
+                if (activeConnections.Contains(1))
+                {
+                    EditVisualActive(i, 1);
+                    break;
+                }
+                break;
+
+            case "wire_cross":
+                activeConnections = CheckConnectionSides(block.inputDirections, GetConnections(i, 0));
+                if (activeConnections.Contains(1))
+                {
+                    EditVisualActive(i, 1);
+                    break;
+                }
+
+                activeConnections = CheckConnectionSides(block.inputDirections, GetConnections(i, 1));
+                if (activeConnections.Contains(1))
+                {
+                    EditVisualActive(i, 1);
+                    break;
+                }
+
+                activeConnections = CheckConnectionSides(block.inputDirections, GetConnections(i, 3));
+                if (activeConnections.Contains(1))
+                {
+                    EditVisualActive(i, 1);
+                    break;
+                }
+                break;
+
+        }
+    }
+
+    public bool IsAnyConnectionActive(int[] directions, int blockIndex, int side)
+    {
+        int[] activeConnections;
+        activeConnections = CheckConnectionSides(directions, GetConnections(blockIndex, side));
+        if (activeConnections.Contains(1))
+        {
+            return true;
+        }
+        return false;
+    }
+
+    public void EditBlockActiveSide(int index, int side, int edit)
+    {
+        blockData block = GetBlock(index);
+        block.outputDirections[side] = edit;
+    }
+
+    public void EditVisualActive(int index, int edit)
+    {
+        blockData block = GetBlock(index);
+        block.visualActive = edit;
+    }
+        
+    public blockData GetBlock(int index)
+    {
+        blockData block = blockData[index];
+        return block;
     }
 
 
-    private int[] checkConnectionSides(int[] connectionSides, List<connections> sources)
+    private int[] CheckConnectionSides(int[] connectionSides, List<connections> sources)
     {
         int[] activeConnectionSides = new int[4] {0,0,0,0};
         for (int side = 0; side < 4; side++)
         {
             if (connectionSides[side] == 1)
             {
-                if (checkActive(sources))
+                if (CheckActive(sources))
                 {
                     activeConnectionSides[side] = 1;
                 }
