99991e78775fef05d16f55d882879a4b98f8eb0a
BaumBenji <bbond3511@gmail.com>
2025-06-25 11:05:08 +0200

    example movement + animation

---
+++ Assets/Example-Scenes/Example-Scripts/Movement.cs
@@ -0,0 +1,51 @@
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    private Animator animator;
+
+    private float Move;
+
+    public float speed;
+    public float jump;
+    public bool grounded;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        animator.SetFloat("Speed", rb.linearVelocity.x);
+        animator.SetFloat("JumpSpeed", rb.linearVelocity.y);
+
+        Move = Input.GetAxisRaw("Horizontal");
+
+        rb.linearVelocity = new Vector2(Move * speed, rb.linearVelocity.y);
+
+        if (Input.GetButtonDown("Jump") && grounded)
+        {
+            rb.AddForce(new Vector2(rb.linearVelocity.x, jump * 10));
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.CompareTag("Ground"))
+        {
+            grounded = true;
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if(collision.gameObject.CompareTag("Ground"))
+        {
+            grounded = false;
+        }
+    }
+}
