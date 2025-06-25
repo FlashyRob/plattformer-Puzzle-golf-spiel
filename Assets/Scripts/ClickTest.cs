e210c00ff72233bc824d5d108f6c686f788ff493
fkroker <fkroker@cjd-koenigswinter.net>
2025-06-25 12:14:49 +0200

    Change selectedMaterial to string

--- Assets/Scripts/ClickTest.cs
+++ Assets/Scripts/ClickTest.cs
@@ -3,20 +3,13 @@ using UnityEngine.EventSystems;
 
 public class ClickTest : MonoBehaviour, IPointerClickHandler
 {
-    public int selectedMaterial;
+    public string selectedMaterial;
     //Detect if a click occurs
     public void OnPointerClick(PointerEventData pointerEventData)
     {
 
         //Output to console the clicked GameObject's name and the following message. You can replace this with your own actions for when clicking the GameObject.
         Debug.Log(name + " Game Object Clicked!");
-        string[] materials = new string[] {"grass", "dirt", "water"};
-        for (int i = 0; i < materials.Length; i++)
-        {
-            if (materials[i] == name)
-            {
-                selectedMaterial = i;
-            }
-        }
+        selectedMaterial = name;
     }
 } 
