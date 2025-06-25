f4ecd5c25aeb0574d2787978797a63e07fef7965
FlashyRob <dmueller@cjd-koenigswinter.net>
2025-06-25 10:42:38 +0200

    W Movement

--- Assets/Scripts/Schmoovement.cs
+++ Assets/Scripts/Schmoovement.cs
@@ -20,32 +20,32 @@ public class NewMonoBehaviourScript : MonoBehaviour
     void Update()
     {
         float horizontal = Input.GetAxis("Horizontal"); // key a pressed = -1 ; key d pressed = 1 ; no key pressed = 0
-        float verticalVelocity = rb2d.linearVelocity.y;
-        float jumpVelocity = 0;
-        float verticalMultiplier;
+        float verticalVelocity; 
+        float jumpVelocity;
 
         if (Input.GetKey(KeyCode.Space))
         {
-            verticalMultiplier = 12;
+            jumpVelocity = 0.01f;
         }
         else
         {
-            verticalMultiplier = 10;
+            jumpVelocity = 0;
         }
 
         if (Input.GetKeyDown(KeyCode.Space) && Grounded)
         {
-            jumpVelocity = 14;
+            jumpVelocity = 10;
         }
 
-        verticalVelocity = jumpVelocity * verticalMultiplier;
+        Debug.Log(rb2d.linearVelocity.y);
+        verticalVelocity = jumpVelocity + (rb2d.linearVelocity.y); 
 
-        rb2d.linearVelocity = new Vector2(horizontal, verticalVelocity);
+        rb2d.linearVelocity = new Vector2(horizontal * 5, verticalVelocity); 
     }
 
-
-    void OnCollisionEnter(Collision2D other)
+    private void OnCollisionEnter2D(Collision2D other)
     {
+        Debug.Log("CollisionEnter");
         if (other.gameObject.CompareTag("Ground"))
         {
             Grounded = true;
@@ -54,6 +54,7 @@ public class NewMonoBehaviourScript : MonoBehaviour
 
     private void OnCollisionExit2D(Collision2D collision)
     {
+        Debug.Log("CollisionExit");
         if (collision.gameObject.CompareTag("Ground"))
         {
             Grounded = false;
