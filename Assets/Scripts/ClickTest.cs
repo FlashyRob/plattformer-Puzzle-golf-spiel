522a1eec2ad1efca2c910894aa9e943b34699644
fkroker <fkroker@cjd-koenigswinter.net>
2025-06-27 14:19:42 +0200

    Remove debug message

--- Assets/Scripts/ClickTest.cs
+++ Assets/Scripts/ClickTest.cs
@@ -4,13 +4,12 @@ using UnityEngine.EventSystems;
 public class ClickTest : MonoBehaviour, IPointerClickHandler
 {
     public static string selectedMaterial = "Nothing";
+    public static bool changed;
     //Detect if a click occurs
     public void OnPointerClick(PointerEventData pointerEventData)
     {
-
-        //Output to console the clicked GameObject's name and the following message. You can replace this with your own actions for when clicking the GameObject.
-        Debug.Log(name + " Game Object Clicked!");
         string[] materials = new string[] {"grass", "dirt", "water"};
         selectedMaterial = name;
+        changed = true;
     }
 } 
