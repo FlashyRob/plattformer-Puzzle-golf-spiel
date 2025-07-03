c72703381e3df0f5c08eefd462e032ad5c236435
fkroker <fkroker@cjd-koenigswinter.net>
2025-07-03 15:30:33 +0200

    Fix scrolling (most of it at least)

--- Assets/Scripts/LevelCreation/LevelEditor.cs
+++ Assets/Scripts/LevelCreation/LevelEditor.cs
@@ -198,9 +198,6 @@ public class LevelEditor : MonoBehaviour
         select.name = "select";
 
         Initialize();
-
-        screenWidthOld = Screen.width;
-        content.GetComponent<RectTransform>().sizeDelta = new Vector2(0, blockSelectorParent.GetComponent<RectTransform>().sizeDelta.y - 70);
     }
 
 
@@ -400,7 +397,7 @@ public class LevelEditor : MonoBehaviour
         if (screenWidthOld != Screen.width)
         {
             screenWidthOld = Screen.width;
-            content.GetComponent<RectTransform>().sizeDelta = new Vector2(0, blockSelectorParent.GetComponent<RectTransform>().sizeDelta.y - 70);
+            content.GetComponent<RectTransform>().sizeDelta = new Vector2(0, blockSelectorParent.GetComponent<RectTransform>().sizeDelta.y);
         }
 
         mousePosOld = mousePos;
